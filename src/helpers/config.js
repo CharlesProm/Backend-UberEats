624b95abd8972c0d23bd59e58a3c1a94566d201d
CharlesProm <carlosjr3112@gmail.com>
2021-07-27 01:10:25 -0400

    database heroku

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg');
-
+require('dotenv').config()
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: 'postgres',
+//     password: '123123',
+//     database: 'uberEats',
+//     port: '5432'
+// })
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '123123',
-    database: 'uberEats',
-    port: '5432'
+    host: 'ec2-44-194-112-166.compute-1.amazonaws.com',
+    user: 'misrzqpedoigfn',
+    password: '6c87ee8a771053f804ec46b6235baccd7e971887be1061ee876de8ef693e2610',
+    database: 'd9emsdqgc7ooch',
+    port: '5432',
+    ssl: {
+        rejectUnauthorized: false,
+      },
 })
 
 
